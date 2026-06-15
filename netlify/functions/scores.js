bcd7220e78aab9aef403ad2ba17e9e222cd23ff1
anadevesa-lab <ana.devesa@gmail.com>
2026-06-15 21:32:06 +0100

    Fix formatting issue in scores.js

--- netlify/functions/scores.js
+++ netlify/functions/scores.js
@@ -1,4 +1,4 @@
-const https = require('https');
+                                                                      const https = require('https');
 
 function get(url, headers) {
           return new Promise((resolve, reject) => {
